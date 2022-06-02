d730c251b820e8d84f78aced6900ee963cb514a5
Florentino Mendes <80356604+Florentinomendes@users.noreply.github.com>
2022-06-02 17:22:22 +0100

    commit

--- js.js
+++ js.js
@@ -1,71 +1,41 @@
 
-//velocidade do jogo= velj, direção do jogador= dirActt, posição do jogador = posAct, elemento da div da tela recebe jog;
+const mario= document.querySelector('.mario');
+const pipa= document.querySelector('.pipa');
+const tela_fim= document.querySelector('.tela_jogo')
+
+const saltar=() => {//Para adicionarmos classe na img mario 
+    mario.classList.add('pular')
+    setTimeout(() => {
+        mario.classList.remove('pular')
+    }, 500);
+}//Definimos0 o método setTimeout para remover a classe pular do css cada vez que o teclado foi acionado
+
+const loop_jogo= setInterval(() => {
+        const pipaPos = pipa.offsetLeft;
+        const marioPos = +window.getComputedStyle(mario).bottom.replace('px', ''); //serve para pegarmos o estilo completado na imagem tirando o px
+        
+        console.log(marioPos);
+        
+        if (pipaPos <= 120 && pipaPos > 0 && marioPos < 80) {
+            
+            pipa.style.animation = 'none';
+            pipa.style.left = `${pipaPos}px`; //Vai ficar parado onde aconteceu a colisão
+            mario.style.animation = 'none';
+            mario.style.bottom = `${marioPos}px`;
+
+            mario.src="3c50b7ea149f7187fc1982cfaad4ae46 (1).webp"
+            mario.style.width= '80px';
+            mario.style.height='100px'
+            mario.style.marginLeft= '30px'
+            clearInterval(loop_jogo);
+           
+        }
+    },10);
 
 
 
-var dirAct_y, dirAct_x ,posAct_x,posAct_y, velj, jog;
-var jogo 
-var frames;
-var tamTelaLargura,tamTelaAltura;
 
 
-function tecla_clicada (){
-var tecla= event.keyCode;
-if(tecla==38){//cima
-    dirAct_y=-1;
-}if(tecla==40){//baixo
-    dirAct_y=1;
-}if(tecla==37){//esquerda
-    dirAct_y=-1;
-}if(tecla==39){//direita
-    dirAct_y=1;
-}
-if(tecla==32){ //barra para atirar
-    
-}
-}
-function tecla_solta (){
-    if((tecla==38)||(tecla==40)){//cima
-        dirAct_y=0;
-    }
-    if((tecla==39)||(tecla==37)){//cima
-        dirAct_y=0;
-    }
-}
-function controlaAtor(){
- posAct_y+= dirAct_y * jog;
- posAct_x+= dirAct_x * jog;
- jog.style.top= posAct_y +"px";
- jog.style.left= posAct_x +"px";
-}
-function gameloop(){
- if(jogo){
-     //funcões do jogo será chamada assim que o jogo virar true
-     controlaAtor();
- }
- frames= requestAnimationFrame(gameloop),
-}
-function inicia(){
-  //Aqui incializamos os componentes do nosso jogo
- jogo= true;
- //incializar jogador
- dirAct_x=dirAct_y=0; // uma forma mais simplificada de atribuir valor aos 2 variáveis.
- posAct_x= tamTelaLargura/2;
- posAct_y= tamTelaAltura/2;
- velj=4;
- jog= document.getElementById("actor_jogo");
- jog.style.top= posAct_y +"px";
- jog.style.left= posAct_x +"px";
 
- gameloop();
+document.addEventListener('keydown', saltar);
 
- 
-  //incializar a tela
-  tamTelaAltura= innerHeight;
-  tamTelaLargura= innerWidth;
-
-
-}
-document.addEventListener("load", inicia);
-document.addEventListener("keydown", tecla_clicada);
-document.addEventListener("keyup", tecla_solta)
